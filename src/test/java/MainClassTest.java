9e109b802d455a8a668b6e2e3f729427567b2e52
Silver <testingbugsrus@gmail.com>
2022-12-04 23:19:21 +0300

    Added Тест 2

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,13 +1,21 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainClassTest {
 	MainClass mainClass = new MainClass();
 
 
 	@Test
+	public void testGetClassNumber() {
+		int expectedNumber = 45;
+		assertTrue(mainClass.getClassNumber() > expectedNumber, "Error, the classNumber's value is less than 45");
+	}
+
+	@Test
 	public void checkLocalNumberTest() {
 		int expectedNumber = 14;
-		Assertions.assertEquals(expectedNumber, mainClass.getLocalNumber(), "Error, the localNumber's value not is 14");
+		assertEquals(expectedNumber, mainClass.getLocalNumber(), "Error, the localNumber's value not is 14");
 	}
 }
